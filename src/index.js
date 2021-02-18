fa056046a0944fed8a5072059a211dbc431d230e
Elena Gubarenko <lenagritsishina@gmail.com>
2021-02-18 20:36:45 +0300

    adding changes

--- src/index.js
+++ src/index.js
@@ -2,35 +2,35 @@ import styles from './styles.css';
 
 const inputChangingThemeRef = document.querySelector('#theme-switch-toggle');
 const bodyRef = document.querySelector('body');
+const checkboxRef = document.querySelector('#theme-switch-toggle');
+console.dir(checkboxRef);
 
 const darkTheme = () => {
   bodyRef.classList.remove('light-theme');
+  localStorage.setItem('theme', 'dark-theme');
   bodyRef.classList.add('dark-theme');
-  localStorage.setItem('theme', JSON.stringify('dark-theme'));
 };
 
 const lightTheme = () => {
   bodyRef.classList.remove('dark-theme');
+  localStorage.setItem('theme', 'light-theme');
   bodyRef.classList.add('light-theme');
-  localStorage.setItem('theme', JSON.stringify('light-theme'));
 };
 
 const savedTheme = () => {
-  // bodyRef.classList.remove('dark-theme');
   bodyRef.classList.add(`${localStorage.getItem('theme')}`);
+  if (`${localStorage.getItem('theme')}` === 'dark-theme') {
+    inputChangingThemeRef.checked = 'true';
+  }
 };
+savedTheme();
 
 const changeTheme = event => {
-  if (localStorage.getItem('theme')) {
-    savedTheme();
-  }
-
-  if (event.type === 'click' && inputChangingThemeRef.checked) {
+  if (inputChangingThemeRef.checked) {
     darkTheme();
-  }
-  if (event.type === 'click' && !inputChangingThemeRef.checked) {
+  } else if (!inputChangingThemeRef.checked) {
     lightTheme();
   }
 };
 
-inputChangingThemeRef.addEventListener('click', changeTheme);
+inputChangingThemeRef.addEventListener('change', changeTheme);
