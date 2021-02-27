5ed37e54fec8ddada83e716b83d1cfe0b566cffe
Elena Gubarenko <lenagritsishina@gmail.com>
2021-02-27 12:42:25 +0300

    adding files

--- src/js_files/template.js
+++
@@ -1,8 +0,0 @@
-import template from '../hbs_files/template.hbs';
-import menuJSON from '../json_files/menu.json';
-
-const ulRef = document.querySelector('.js-menu');
-const markup = template(menuJSON);
-ulRef.insertAdjacentHTML('beforeend', markup);
-
-console.log(menuJSON);
